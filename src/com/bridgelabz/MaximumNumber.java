36f6e134c01fbf128eea25170070a5d4c59e4c84
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-18 09:31:14 +0530

    Added :given 3 integers find the maximum

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -1,8 +1,20 @@
 package com.bridgelabz;
 
 public class MaximumNumber {
+    public void integerMax(int x, int y, int z) {
+        if (x > y && x > z) {
+                System.out.println("Maximum Integers is = " + x);
+            }
+        if (y > x && y > z) {
+                System.out.println("Maximum Integers is = " + y);
+            }
+        if (z > x && z > y) {
+                System.out.println("Maximum Integers is = " + z);
+            }
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Maximum number");
+        MaximumNumber obj=new MaximumNumber();
+        obj.integerMax(10,20,30);
     }
 
 }
