1a3a99b1b7e8768d105afa86d27b00366052b985
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2023-04-26 17:56:10 +0530

    Given three floats find maximum

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -1,20 +1,18 @@
 package com.bridgelabz;
 
 public class MaximumNumber {
-    public void floatMax(float x, float y, float z) {
-        if (x > y && x > z) {
-                System.out.println("Maximum Float is = " + x);
-            }
-        if (y > x && y > z) {
-                System.out.println("Maximum Float is = " + y);
-            }
-        if (z > x && z > y) {
-                System.out.println("Maximum Float is = " + z);
-            }
+    public static void floatMax(float x, float y, float z) {
+        Float max=x;
+        if (y.compareTo(max)>0){
+                max=y;
+        }
+        if (z.compareTo(max)>0){
+               max=z;
+        }
+        System.out.println(max+" is maximum");
     }
     public static void main(String[] args) {
-        MaximumNumber obj=new MaximumNumber();
-        obj.floatMax(10.4f,20.6f,30.9f);
+        floatMax(10.4f,20.6f,30.9f);
     }
 
 }
