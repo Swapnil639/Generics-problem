590c3944776a98c7d0d8200e16e8e6de083cd917
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2023-04-26 18:01:56 +0530

    Added:Given three String find maximum

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -3,24 +3,19 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class MaximumNumber {
-    public void StringMax(String x, String y, String z) {
-        if (x.length()>y.length() && x.length()>z.length()) {
-                System.out.println("Maximum String is = " + x);
-            }
-        if (y.length() > x.length() && y.length() > z.length()) {
-                System.out.println("Maximum String is = " + y);
-            }
-        if (z.length() > x.length() && z.length() > y.length()) {
-                System.out.println("Maximum String is = " + z);
-            }
+    public static void stringMax(String x,String y,String z){
+        String max=x;
+        if (y.compareTo(max)>0){
+                max=y;
+        }
+        if (z.compareTo(max)>0){
+               max=z;
+        }
+        System.out.println(max+" is maximum");
     }
+
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        MaximumNumber obj=new MaximumNumber();
-        String x=scanner.next();
-        String y=scanner.next();
-        String z=scanner.next();
-        obj.StringMax(x,y,z);
+        stringMax("Swapnil","Shubham","Kunal");
     }
 
 }
